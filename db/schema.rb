ef91956bcccc409f335035ebab2bf8d33f8ca980
unknown <tristau@nwilson-HP.digitalinsurance.com>
2015-06-08 15:00:59 -0400

    init

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150528180725) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: true do |t|
     t.string   "title"
     t.string   "picture"
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 20150528180725) do
     t.datetime "updated_at"
   end
 
-  add_index "notes", ["location_id"], name: "index_notes_on_location_id"
+  add_index "notes", ["location_id"], name: "index_notes_on_location_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -51,7 +54,7 @@ ActiveRecord::Schema.define(version: 20150528180725) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
